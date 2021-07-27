2c0b7b4f41b357db52ded9aa48f964c7c318f234
Atryom-Bohaturou <81459880+Atryom-Bohaturou@users.noreply.github.com>
2021-07-27 15:29:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import Display from './components/DisplayContainer';
 function App() {
   return (
     <div className="App">
-
-
       <InputFields />
-
       <Display />
+      <a href="https://github.com/Atryom-Bohaturou/Tip-Calculator">"Repository of this project"</a>
     </div>
   );
 }
